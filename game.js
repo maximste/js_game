936466288e149e0f1382c4676c6240b24aa2a027
Maxim Steinbrecher <maximste@gmail.com>
2017-09-26 14:25:58 +0300

    First version of game.js

--- game.js
+++ game.js
@@ -1,6 +1,13 @@
 'use strict';
 /*
 	Класс Vector создает объект со свойствами x и y, равными переданным в конструктор координатам.
+	
+	Метод plus принимает один аргумент — вектор, объект Vector.
+	Если передать аргумент другого типа, то бросает исключение "Можно прибавлять к вектору только вектор типа Vector".
+	Создает и возвращает новый объект типа Vector, координаты которого будут суммой соответствующих координат суммируемых векторов.
+
+	Метод times gринимает один аргумент — множитель, число.
+	Создает и возвращает новый объект типа Vector, координаты которого будут равны соответствующим координатам исходного вектора, умноженным на множитель.
 */
 class Vector {
 	constructor(x = 0, y = 0) {
@@ -8,11 +15,6 @@ class Vector {
 		this.y = y;
 	}
 
-	/*
-		Принимает один аргумент — вектор, объект Vector.
-		Если передать аргумент другого типа, то бросает исключение "Можно прибавлять к вектору только вектор типа Vector".
-		Создает и возвращает новый объект типа Vector, координаты которого будут суммой соответствующих координат суммируемых векторов.
-	*/
 	plus(vector) {
 		if (vector instanceof Vector) {
 			return new Vector(this.x + vector.x, this.y + vector.y);
@@ -21,24 +23,34 @@ class Vector {
 		}    
   }
 
-	/*
-	  Принимает один аргумент — множитель, число.
-	  Создает и возвращает новый объект типа Vector, координаты которого будут равны соответствующим координатам исходного вектора, умноженным на множитель.
-	*/
   times(multiplier) {
     return new Vector(this.x * multiplier, this.y * multiplier);
   }
 }
 
 /*
-	Класс Actor позволяет контролировать все движущиеся объекты на игровом поле и контролировать их пересечение.
+	Класс Actor, который позволит контролировать все движущиеся объекты на игровом поле и контролировать их пересечение.
+	Конструктор принимает три аргумента - объекты типа Vector: расположение (pos), размер (size) и скорость (speed).
+	По умолчанию создается объект с координатами 0:0, размером 1x1 и скоростью 0:0. 
+	Если в качестве первого, второго или третьего аргумента передать не объект типа Vector, то конструктор бросает исключение.
+	
+	Свойства:
+	- pos, в котором размещен Vector. 
+	- size, в котором размещен Vector.
+	- speed, в котором размещен Vector.
+	Определен метод act, который ничего не делает.
+	Определены свойства только для чтения left, top, right, bottom, в которых установлены границы объекта по осям X и Y с учетом его расположения и размера.
+	Свойство type — строка со значением actor, только для чтения.
+	
+	Метод isIntersect проверяет, пересекается ли текущий объект с переданным объектом, и если да, возвращает true, иначе false.
+	Принимает один аргумент — движущийся объект типа Actor. Если передать аргумент другого типа или вызвать без аргументов, то метод бросает исключение. Если передать в качестве аргумента этот же объект, то всегда возвращает false. Объект не пересекается сам с собой. Объекты, имеющие смежные границы, не пересекаются.
 */
 class Actor {
 	constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
 		this.pos = pos;
 		this.size = size;
 		this.speed = speed;
-		
+
 		if (!(this.pos instanceof Vector) || !(this.size instanceof Vector) || !(this.speed instanceof Vector)) {
 			throw new Error('Расположение, размер и скорость должны быть объектами типа Vector');
 		}			
@@ -81,4 +93,55 @@ class Actor {
       actor.bottom > this.top &&
       actor.top < this.bottom;
   }
+}
+
+class Level {
+	constructor(grid, actors) {
+		this.grid = grid;
+		this.actors = actors; //верно, не удаляй
+		this.height = grid.length;
+		this.width = grid[0].length;
+		this.status = null; //верно, не удаляй
+		this.finishDelay = 1; //верно, не удаляй
+	}
+
+	isFinished() {
+		return this.status !== null && this.finishDelay < 0; //верно, не удаляй
+	}
+}
+const grid = [
+  [undefined, undefined],
+  ['wall', 'wall']
+];
+
+function MyCoin(title) {
+  this.type = 'coin';
+  this.title = title;
+}
+MyCoin.prototype = Object.create(Actor);
+MyCoin.constructor = MyCoin;
+
+const goldCoin = new MyCoin('Золото');
+const bronzeCoin = new MyCoin('Бронза');
+const player = new Actor();
+const fireball = new Actor();
+
+const level = new Level(undefined, [ goldCoin, bronzeCoin, player, fireball ]);
+
+level.playerTouched('coin', goldCoin);
+level.playerTouched('coin', bronzeCoin);
+
+if (level.noMoreActors('coin')) {
+  console.log('Все монеты собраны');
+  console.log(`Статус игры: ${level.status}`);
+}
+
+const obstacle = level.obstacleAt(new Vector(1, 1), player.size);
+if (obstacle) {
+  console.log(`На пути препятствие: ${obstacle}`);
+}
+
+const otherActor = level.actorAt(player);
+if (otherActor === fireball) {
+  console.log('Пользователь столкнулся с шаровой молнией');
 }
