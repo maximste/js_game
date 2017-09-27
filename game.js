a490c691cc21bc3c3c60f1df197870a8182d60f5
Maxim Steinbrecher <maximste@gmail.com>
2017-09-27 14:18:45 +0300

    Added script to run the game

--- game.js
+++ game.js
@@ -289,3 +289,17 @@ class Player extends Actor {
 		return 'player';
 	}
 }
+
+const actorDict = {
+  '@': Player,
+  'o': Coin,
+  'v': FireRain,
+  '|': VerticalFireball,
+  '=': HorizontalFireball
+};
+
+const parser = new LevelParser(actorDict);
+
+loadLevels().then(levels => {
+  return runGame(JSON.parse(levels), parser, DOMDisplay)
+}).then(result => alert('Вы выиграли!'));
